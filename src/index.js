b2aa5f0ce07598864bf3ce55aa6fb79d944d4996
Greghori101 <e.souala@esi-sba.dz>
2022-12-25 01:05:48 +0100

    DEC-25

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./index.css";
+
+// layouts
+
+// views without layouts
+
+import Landing from "./Views/Landing.js";
+import Profile from "./Views/Profile.js";
+import Index from "./Views/Index.js";
+import Error404 from "./Views/Errors/Error404";
+
+
+//const cors = require('cors')
+
+//app.use(cors())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <BrowserRouter>
+    <Routes>
+      
+      {/* add routes without layouts */}
+      <Route path="/landing" exact element={<Landing/>} />
+      <Route path="/profile" exact element={<Profile/>} />
+      <Route path="/" exact element={<Index/>} />
+      <Route path="/404" exact element={<Error404/>} />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      {/* Page Not Found */}
+      <Route path="*" element={<Navigate to="/404" />}/>
+      
+    </Routes>
+  </BrowserRouter>
+);
