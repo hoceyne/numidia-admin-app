fdf9ee4397b20d96f57931b7c15a76a46ab903a3
Greghori101 <e.souala@esi-sba.dz>
2023-01-01 09:43:22 +0100

    JAN-1

--- src/Components/Navbars/IndexNavbar.js
+++ src/Components/Navbars/IndexNavbar.js
@@ -14,7 +14,7 @@ export default function Navbar(props) {
 							to="/"
 							className="text-blueGray-700 text-xl font-bold leading-relaxed flex items-center mr-4 py-2 whitespace-nowrap capitalize"
 						>
-							
+							<img src="../../assets/img/logo/numidia-school-logo.svg"></img>
 							Numidia
 						</Link>
 						<button
